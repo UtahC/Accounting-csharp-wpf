c8b63afb84db8fefaf87548b84db33128a08bc7b
UtahC <utah9527@gmail.com>
2016-05-22 18:17:07 +0800

    Add Item.cs

---
+++ Accounting/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting
+{
+    public class Item
+    {
+        public Item()
+        {
+
+        }
+        public Item(string time, string cusName, string itemName, int count, int weight, int price, string note)
+        {
+            this.time = time;
+            this.cusName = cusName;
+            this.itemName = itemName;
+            this.count = count;
+            this.weight = weight;
+            this.price = price;
+            this.note = note;
+        }
+        public Item(int id, string time, string cusName, string itemName, int count, int weight, int price, string note) {
+            this.id = id;
+            this.time = time;
+            this.cusName = cusName;
+            this.itemName = itemName;
+            this.count = count;
+            this.weight = weight;
+            this.price = price;
+            this.note = note;
+        }
+        public int id { get; set; }
+        public string time { get; set; }
+        public string cusName { get; set; }
+        public string itemName { get; set; }
+        public int count { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+        public string note { get; set; }
+    }
+}
